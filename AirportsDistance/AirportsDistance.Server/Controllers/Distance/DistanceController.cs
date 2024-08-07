4efde4485f9fa8cdc7ce182c251af85df81bb971
Stanislav <skipper3191@yandex.ru>
2024-08-07 22:36:05 +0300

    add DistanceController

---
+++ AirportsDistance/AirportsDistance.Server/Controllers/Distance/DistanceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirportsDistance.Server.Controllers.Distance
+{
+	public class DistanceController : BaseController
+	{
+		public DistanceController(ILogger<BaseController> logger) : base(logger)
+		{
+		}
+
+		[HttpGet]
+		public ActionResult<double> Get(double latitude1, double longitude1, double latitude2, double longitude2)
+		{
+			return Ok(15.0);
+		}
+	}
+}
