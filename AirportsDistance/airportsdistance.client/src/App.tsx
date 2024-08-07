bf0ce1c63ba8f02c296d1b52b1848c5f0d8cb5e7
Stanislav <skipper3191@yandex.ru>
2024-08-07 22:13:10 +0300

    refactoring client

--- AirportsDistance/airportsdistance.client/src/App.tsx
+++ AirportsDistance/airportsdistance.client/src/App.tsx
@@ -1,56 +1,28 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
-
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+	const [distance, setDistance] = useState<number>(0);
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+	useEffect(() => {
+		getDistance();
+	}, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+	const contents = <strong><p>Distance: {distance}</p></strong>;
 
-    return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
+	return (
+		<div>
+			<h1 id="tableLabel">Airports Distance</h1>
+			<p>This component demonstrates fetching data from the server.</p>
+			{contents}
+		</div>
+	);
 
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+	async function getDistance() {
+		const response = await fetch('distance');
+		const data = await response.json();
+		setDistance(data);
+	}
 }
 
 export default App;
