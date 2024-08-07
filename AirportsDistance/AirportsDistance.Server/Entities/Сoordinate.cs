5f1122ac0595018d909fea68901a43cfa2429126
Stanislav <skipper3191@yandex.ru>
2024-08-07 22:56:00 +0300

    add attribute for parse json

--- AirportsDistance/AirportsDistance.Server/Entities/Сoordinate.cs
+++ AirportsDistance/AirportsDistance.Server/Entities/Сoordinate.cs
@@ -1,8 +1,13 @@
-﻿namespace AirportsDistance.Server.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace AirportsDistance.Server.Entities
 {
 	public class Coordinate
 	{
+		[JsonPropertyName("lat")]
 		public double Latitude { get; set; }
+
+		[JsonPropertyName("lon")]
 		public double Longitude { get; set; }
 	}
 }
