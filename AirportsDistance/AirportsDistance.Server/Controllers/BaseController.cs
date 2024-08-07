2c69655792f7164a7c1e229931316e0aa4d07975
Stanislav <skipper3191@yandex.ru>
2024-08-07 22:12:27 +0300

    Add BaseController

---
+++ AirportsDistance/AirportsDistance.Server/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirportsDistance.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public abstract class BaseController : ControllerBase
+	{
+		private readonly ILogger<BaseController> _logger;
+
+		public BaseController(ILogger<BaseController> logger)
+		{
+			_logger = logger;
+		}
+	}
+}
