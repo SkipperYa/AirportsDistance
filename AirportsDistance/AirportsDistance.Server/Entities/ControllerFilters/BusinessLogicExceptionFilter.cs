e78667abf6f2f30be25b35c708868d50dab2bd11
Stanislav <skipper3191@yandex.ru>
2024-08-07 22:36:51 +0300

    add ccustom exception and add global filter for controllers

---
+++ AirportsDistance/AirportsDistance.Server/Entities/ControllerFilters/BusinessLogicExceptionFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AirportsDistance.Server.Entities.ControllerFilters
+{
+	public class BusinessLogicExceptionFilter : Attribute, IExceptionFilter
+	{
+		public void OnException(ExceptionContext context)
+		{
+			// throw new NotImplementedException();
+		}
+	}
+}
---
+++ AirportsDistance/AirportsDistance.Server/Entities/CustomException/BusinessLogicException.cs
@@ -0,0 +1,13 @@
+﻿namespace AirportsDistance.Server.Entities.CustomException
+{
+	/// <summary>
+	/// Exception for send to client error text
+	/// </summary>
+	public class BusinessLogicException : Exception
+	{
+		public BusinessLogicException(string message) : base(message)
+		{
+
+		}
+	}
+}
