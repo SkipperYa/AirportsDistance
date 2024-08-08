90ea28e153c57f26e9860a3fc9b4bb5461a52242
Stanislav <skipper3191@yandex.ru>
2024-08-08 12:01:06 +0300

    add bootstrap and improve client

--- AirportsDistance/airportsdistance.client/src/App.tsx
+++ AirportsDistance/airportsdistance.client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface Response<T> {
 	error: string;
@@ -17,21 +18,59 @@ function App() {
 	const contents = <strong><p>Distance: {distance} miles</p></strong>;
 
 	return (
-		<div>
+		<div className="conteiner">
 			<h1>Airports Distance</h1>
-			<p>This component demonstrates fetching data from the server.</p>
-			<div>
-				<button
-					className="button"
-					type="button"
-					onClick={async () => {
-						await getDistance();
-					}}
-				>
-					Get Distance
-				</button>
+			<hr />
+			<p>REST service to measure distance in miles between two airports.</p>
+			<br />
+			<div className="row">
+				<div className="col-sm">
+					<div className="input-group mb-3">
+						<div className="input-group-prepend">
+							<span className="input-group-text" id="inputGroup-sizing-default">IATA 1</span>
+						</div>
+						<input
+							type="text"
+							className="form-control"
+							onChange={(e) => {
+								setIata1(e.target.value);
+							}}
+						/>
+					</div>
+				</div>
+				<div className="col-sm">
+					<div className="input-group mb-3">
+						<div className="input-group-prepend">
+							<span className="input-group-text" id="inputGroup-sizing-default">IATA 2</span>
+						</div>
+						<input
+							type="text"
+							className="form-control"
+							onChange={(e) => {
+								setIata2(e.target.value);
+							}}
+						/>
+					</div>
+				</div>
+				<div className="col-sm">
+					<button
+						className="btn btn-dark"
+						type="button"
+						onClick={async () => {
+							await getDistance();
+						}}
+					>
+						Get Distance
+					</button>
+				</div>
 			</div>
-			{contents}
+			<br />
+			<div className="text-center">
+				{contents}
+			</div>
+			{error && <div className="text-danger">
+				{error}
+			</div>}
 		</div>
 	);
 
